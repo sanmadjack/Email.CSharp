b12576b8f7010abc6a4a0650617cdec0cbbc94d2
Matthew Barbour <sanmadjack@masgau.org>
2012-05-11 11:10:07 -0500

    Cleanup and formatting

--- EmailHandler.cs
+++ EmailHandler.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using System.ComponentModel;
 using System.Net.Mail;
-using System.ComponentModel;
 
-namespace Email
-{
-    public class EmailHandler: BackgroundWorker
-    {
+namespace Email {
+    public class EmailHandler : BackgroundWorker {
         private readonly string server;
         private readonly string email_sender;
         private readonly string email_password;
@@ -23,10 +20,9 @@ namespace Email
         }
 
         public bool email_available = false;
-        private void checkAvailability(object sender, System.ComponentModel.DoWorkEventArgs e)
-        {
+        private void checkAvailability(object sender, System.ComponentModel.DoWorkEventArgs e) {
             try {
-                System.Net.Sockets.TcpClient clnt=new System.Net.Sockets.TcpClient(server,587);
+                System.Net.Sockets.TcpClient clnt = new System.Net.Sockets.TcpClient(server, 587);
                 clnt.Close();
                 email_available = true;
             } catch {
@@ -35,7 +31,7 @@ namespace Email
         }
 
         private string from, to, reply_to, body, title;
-        public void sendEmail(string new_from, string new_to, string new_reply_to, string new_title,string new_body, RunWorkerCompletedEventHandler target) {
+        public void sendEmail(string new_from, string new_to, string new_reply_to, string new_title, string new_body, RunWorkerCompletedEventHandler target) {
             this.DoWork += new System.ComponentModel.DoWorkEventHandler(sendEmail);
             RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(target);
             from = new_from;
@@ -54,7 +50,7 @@ namespace Email
             mail.To.Add(to);
             mail.Subject = title;
             mail.ReplyToList.Add(new MailAddress(from));
-                
+
             //AlternateView planview = AlternateView.CreateAlternateViewFromString("This is my plain text content, viewable tby those clients that don't support html");
             //AlternateView htmlview = AlternateView.CreateAlternateViewFromString("<b>This is bold text and viewable by those mail clients that support html<b>");
             // mail.AlternateViews.Add(planview);
@@ -64,15 +60,14 @@ namespace Email
             mail.Priority = MailPriority.High;
             mail.Headers.Add("Disposition-Notification-To", "<" + email_sender + ">");
             // mail.Attachments.Add(Server.MapPath("/"));
-            SmtpClient smtp = new SmtpClient(server, 587)
-            {
+            SmtpClient smtp = new SmtpClient(server, 587) {
                 Credentials = new System.Net.NetworkCredential(email_sender, email_password),
                 EnableSsl = true
             };
             smtp.Send(mail);
         }
         public static bool validateEmailAddress(string email) {
-            if(email.Contains("@"))
+            if (email.Contains("@"))
                 return true;
             else
                 return false;

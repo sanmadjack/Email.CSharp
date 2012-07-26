8a0bfb515cfe9faacde005b2b3da3bb4f56387b7
Matthew Barbour <sanmadjack@gmail.com>
2012-07-26 17:52:03 -0500

    Added generic email source interface

---
+++ IEmailSource.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Email {
+    public interface IEmailSource {
+        string EmailSender { get; set; }
+        string EmailRecipient { get; set; }
+    }
+}
